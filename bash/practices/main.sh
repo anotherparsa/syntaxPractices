4b0ac0a5296abf8f28c2af4fd0ac3a593da64ebd
anotherparsa <anotherparsa@gmail.com>
2024-06-25 22:40:41 +0330

    bash

--- bash/practices/main.sh
+++ bash/practices/main.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #this is commenting
-echo "This is the first script"
+echo "This script is running in $(pwd)"
 name="Sara"
 echo "Hello ${name}"
 time=$(date +%H:%m:%S)
 echo "Time is ${time}"
+echo "Using Array"
+Array=(one two three four "This is five")
+echo "${Array[0]}"
+echo "${Array[4]}"
 exit 0
 
