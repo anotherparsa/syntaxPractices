b46fdadfb11fa68d4de1c76eafe5cef4f9bec486
anotherparsa <anotherparsa@gmail.com>
2024-06-26 01:31:59 +0330

    #bash

--- bash/practices/main.sh
+++ bash/practices/main.sh
@@ -1,14 +1,6 @@
 #/bash/bin
 
-read -p "Enter your age: " age
-echo "You're ${age} years old"
-if [ $age -ge 18 ];
-then
-	echo "You're in legal age"
-else
-	echo "You're not in legal age" 
-fi
-while [ $age -gt 0 ]; do
-	echo "Happy birth day your ${age}"
-	age=$(( $age - 1 ))
+array=(one two three four five six seven eigth nine ten)
+for element in "${array[@]}"; do
+	echo "This is $element"
 done
