d6a5cfc539e43ff6ebe26123f9e1040f35ca63df
anotherparsa <anotherparsa@gmail.com>
2024-06-26 01:26:13 +0330

    #bash

--- bash/practices/main.sh
+++ bash/practices/main.sh
@@ -2,9 +2,13 @@
 
 read -p "Enter your age: " age
 echo "You're ${age} years old"
-if [ $age -ge 18 ]
+if [ $age -ge 18 ];
 then
 	echo "You're in legal age"
 else
 	echo "You're not in legal age" 
 fi
+while [ $age -gt 0 ]; do
+	echo "Happy birth day your ${age}"
+	age=$(( $age - 1 ))
+done
