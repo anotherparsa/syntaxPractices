512ea757482e12955ab83fce1041bb5bd45fc8c1
anotherparsa <anotherparsa@gmail.com>
2024-06-29 22:48:27 +0330

    #cpp

--- cpp/Stack/main.cpp
+++ cpp/Stack/main.cpp
@@ -18,24 +18,32 @@ class Stack{
         Node* top;
         Node* bottom;
         int length;
+        int capacity;
 
     public:
         Stack(){
             this->top = nullptr;
             this->bottom = nullptr;
             this->length = 0;
+            this->capacity = 0;
         }
 
         void start(){
+            cout << "Please enter the stack capacity: ";
+            cin >> this->capacity;
             while(true){
                 cout << "Stack management center: " << endl << 
                 "choose an option below: " << endl << 
-                "1-print top     2-print bottom     3-print length     4-print stack " << endl << 
-                "5-print specific Node value by index     6-set specific Node value     7-push a Node     8-pop a Node" << endl;
+                "1-print top     2-print bottom     3-print length     4-print capcity " << endl << 
+                "5-print stack     6-print stack     7-push a Node     8-pop a Node" << endl;
                 int input;
                 cin >> input;
                 switch (input){
-                    case 1 : this->print_head(); break;
+                    case 1 : this->print_top(); break;
+                    case 2 : this->print_bottom(); break;
+                    case 3 : this->print_length(); break;
+                    case 4 : this->print_capacity(); break;
+
 
                     default : cout << "No valid option"; break;
 
@@ -45,7 +53,7 @@ class Stack{
         }
 
         bool is_stack_empty(){
-            return (this->top == nullptr || this->length == 0)
+            return (this->top == nullptr || this->length == 0);
         }
 
         void print_top(){
@@ -64,8 +72,23 @@ class Stack{
             }
         }
 
-        
+        void print_length(){
+            cout << "Length: " << this->length << endl;
+        }
+
+        void print_capacity(){
+            cout << "Capacity " << this->capacity << endl;
+        }
+
+
 };
+
+
+int main(){
+    Stack* my_stack = new Stack();
+    my_stack->start();
+
+}
 
 
 
