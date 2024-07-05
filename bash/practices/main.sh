534ac42c0fa14b873c0f0d2a1a9548abb66c260b
anotherparsa <anotherparsa@gmail.com>
2024-07-06 02:16:34 +0330

    #dart

--- bash/practices/main.sh
+++
@@ -1,6 +0,0 @@
-#/bash/bin
-
-array=(one two three four five six seven eigth nine ten)
-for element in "${array[@]}"; do
-	echo "This is $element"
-done
--- bash/projects/AgeChecker/main.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-#validating age
-echo "You're $1 Years old"
-if [ $1 -ge 20 ]
-then
-	echo "You're in legal age"
-else
-	echo "You're not in legal age"
-fi
