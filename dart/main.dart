2bfc666cfbbf1e171d1647ca9d7866522e39f256
anotherparsa <anotherparsa@gmail.com>
2024-07-06 20:45:00 +0330

    #dart

--- dart/main.dart
+++ dart/main.dart
@@ -1,18 +1,21 @@
-void main(){
-	print("Hello World");
-	//this is a comment =)
-	print(43+34);
-	int age = 22;
-	print(age);
-	dynamic dynamicvalue = 10;
-	print(dynamicvalue);
-	dynamicvalue = "This is string";
-	print(dynamicvalue);
-	dynamicvalue = false;
-	print(dynamicvalue);
-	dynamicvalue = 10.25;
-	print(dynamicvalue);
-	String name = "Name";
-	String greeting = "Hello ";
-	print('${greeting}${name}');
+void main() {
+  print("Hello World");
+  //this is a comment =)
+  print(43 + 34);
+  int age = 22;
+  print(age);
+  dynamic dynamicvalue = 10;
+  print(dynamicvalue);
+  dynamicvalue = "This is string";
+  print(dynamicvalue);
+  dynamicvalue = false;
+  print(dynamicvalue);
+  dynamicvalue = 10.25;
+  print(dynamicvalue);
+  String name = "Name";
+  String greeting = "Hello ";
+  print('${greeting}${name}');
+  var somevalue = 10;
+  print(somevalue);
+  print(DateTime.now());
 }
