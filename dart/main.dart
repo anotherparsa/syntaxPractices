c547f982386c2c1914ebad97db60ef3dfa28b2e2
anotherparsa <anotherparsa@gmail.com>
2024-07-07 01:54:36 +0330

    #dart

--- dart/main.dart
+++ dart/main.dart
@@ -5,11 +5,16 @@ void main(){
 	print(cookie1.shape);
 	cookie1.shape = "square";
 	print(cookie1.shape);
+	Cookie cookie2 = Cookie();
 }
 
 class Cookie{
 	String shape = "circle";
 	double size = 15.2;
+
+	Cookie(){
+		print("Constructor has been executed");
+	}
 
 	void baking(){
 		print("Baking has started");
