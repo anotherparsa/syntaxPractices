dedce69220eda1cb3b00a8738963b46521df0778
anotherparsa <anotherparsa@gmail.com>
2024-07-06 03:39:45 +0330

    #dart

--- dart/main.dart
+++ dart/main.dart
@@ -4,4 +4,12 @@ void main(){
 	print(43+34);
 	int age = 22;
 	print(age);
+	dynamic dynamicvalue = 10;
+	print(dynamicvalue);
+	dynamicvalue = "This is string";
+	print(dynamicvalue);
+	dynamicvalue = false;
+	print(dynamicvalue);
+	dynamicvalue = 10.25;
+	print(dynamicvalue);
 }
