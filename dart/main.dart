0dde7254a75a047ed6a4f4b375c85ad81fbb5cc8
anotherparsa <anotherparsa@gmail.com>
2024-07-09 00:28:21 +0330

    #dart

--- dart/main.dart
+++ dart/main.dart
@@ -1,17 +1,37 @@
 void main(){
-	Car new_car = Car();
-	new_car.accelerate(29);
-	new_car.display_engine();
+	Car new_car = Car("V6", 200, 45000);
+	new_car.display_top_speed();
+	new_car.display_engine_type();
+	new_car.display_price();
+
 }
 
 class Car{
-	String engine = "V6";
+	String? engine_type;
+	int? top_speed;
+	int? price;
+
+
+	Car(String engine_type, int top_speed, int price){
+		print("A car has been made");
+		this.engine_type = engine_type;
+		this.top_speed = top_speed;
+		this.price = price;
+	}
 
 	void accelerate(int acceleration){
 		print("accelerating ${acceleration} KM/H");
 	}
 
-	void display_engine(){
-		print("engine is ${this.engine}");
+	void display_engine_type(){
+		print("engine is ${this.engine_type}");
+	}
+
+	void display_price(){
+		print("Price is ${this.price}");
+	}
+
+	void display_top_speed(){
+		print("Top speed is ${this.top_speed}");
 	}
 }
