0acae2d5f8343845107da15c2922c28cb2a8ded6
anotherparsa <anotherparsa@gmail.com>
2024-07-08 07:50:57 +0330

    #dart

--- dart/main.dart
+++ dart/main.dart
@@ -1,29 +1,7 @@
 void main(){
-	Cookie cookie1 = Cookie("Circle", 16.3);
-	print(cookie1.is_cooling());
-	print(cookie1.shape);
-	cookie1.shape = "square";
-	print(cookie1.shape);
-	Cookie cookie2 = Cookie("Circle", 20.14);
-	print(cookie2.shape);
+	print(addNumber(1,2));
 }
 
-class Cookie{
-	String? shape;
-	double? size;
-
-	Cookie(String shape, double size){
-		print("Constructor has been executed");
-		this.shape = shape;
-		this.size = size;
-		this.baking();
-	}
-
-	void baking(){
-		print("Baking has started");
-	}
-
-	bool is_cooling(){
-		return false;
-	}
+int addNumber(int n1, int n2){
+	return n1 + n2;
 }
