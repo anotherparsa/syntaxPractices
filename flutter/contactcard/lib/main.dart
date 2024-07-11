7465c5a159cd9e8664bab192cb60a09811a4e449
anotherparsa <anotherparsa@gmail.com>
2024-07-11 03:31:28 +0330

    #flutter new practice

---
+++ flutter/contactcard/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text("This is going to be contact card"),
+      ),
+      appBar: AppBar(
+        title: Text("Contact Card"),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+    ),
+  ));
+}
