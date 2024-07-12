c279e32a07747e47b3fbb55969dd1c730b093755
anotherparsa <anotherparsa@gmail.com>
2024-07-12 03:50:00 +0330

    #flutter working with row widget

--- flutter/contactcard/lib/main.dart
+++ flutter/contactcard/lib/main.dart
@@ -9,14 +9,30 @@ void main() {
           Container(
             width: 100,
             height: 100,
-            child: Text("1ST"),
+            child: Text("1ST in column"),
             color: Colors.red,
           ),
           Container(
             width: 100,
             height: 100,
-            child: Text("2ND"),
+            child: Text("2ND in column"),
             color: Colors.blue,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                child: Text("1ST in row"),
+                color: Colors.pink,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                child: Text("2ND in row"),
+                color: Colors.purple,
+              )
+            ],
           )
         ],
       )),
