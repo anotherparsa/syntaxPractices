8ab7a2e3f048802bb29d514fb2e712ab089bf2c8
anotherparsa <anotherparsa@gmail.com>
2024-07-13 01:19:24 +0330

    #flutter disabling debug banner

--- flutter/contactcard/lib/main.dart
+++ flutter/contactcard/lib/main.dart
@@ -1,47 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Center(
-          child: Column(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            child: Text("1ST in column"),
-            color: Colors.red,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            child: Text("2ND in column"),
-            color: Colors.blue,
-          ),
-          Row(
-            children: [
-              Container(
-                width: 50,
-                height: 50,
-                child: Text("1ST in row"),
-                color: Colors.pink,
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                child: Text("2ND in row"),
-                color: Colors.purple,
-              )
-            ],
-          )
-        ],
-      )),
-      appBar: AppBar(
-        title: Text("Contact Card"),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+            child: Column(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              child: Text("1ST in column"),
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              child: Text("2ND in column"),
+              color: Colors.blue,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  child: Text("1ST in row"),
+                  color: Colors.pink,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  child: Text("2ND in row"),
+                  color: Colors.purple,
+                )
+              ],
+            )
+          ],
+        )),
+        appBar: AppBar(
+          title: Text("Contact Card"),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+        ),
         backgroundColor: Colors.white,
-        centerTitle: true,
       ),
-      backgroundColor: Colors.white,
     ),
-  ));
+  );
 }
