57636b09d12d456fc8cb4a6e1ad5f19968ca09cf
anotherparsa <anotherparsa@gmail.com>
2024-07-13 05:38:22 +0330

    #flutter working with padding widget

---
+++ flutter/practicebox/android/app/src/main/kotlin/com/example/practicebox/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.practicebox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter/practicebox/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Practice Box"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 200),
+              child: Text("This is a text 1"),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 200),
+              child: Text("This is a text 2"),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.white,
+    ),
+  ));
+}
