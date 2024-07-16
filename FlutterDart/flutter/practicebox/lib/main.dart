50b17203612870e4ee0986c29696da996366f5fd
anotherparsa <anotherparsa@gmail.com>
2024-07-16 07:02:50 +0330

    #flutter logo

--- FlutterDart/flutter/practicebox/lib/main.dart
+++ FlutterDart/flutter/practicebox/lib/main.dart
@@ -9,12 +9,13 @@ void main() {
         title: Text("Practice Box"),
         backgroundColor: Colors.blue,
       ),
-      body:  Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text("Text 1", style: TextStyle(color: Colors.blue, fontSize: 50),),
           Text("Text 2", style: TextStyle(color: Colors.red, fontSize: 50),),
-          Text("Text 3", style: TextStyle(color: Colors.green, fontSize: 50),)
+          Text("Text 3", style: TextStyle(color: Colors.green, fontSize: 50),),
+          FlutterLogo(size: 250,)
         ],
       ),
       backgroundColor: Colors.white,
