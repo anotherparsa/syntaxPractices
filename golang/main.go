2c5c1234365c69f02f8b127ce31b9064c60f4c73
anotherparsa <anotherparsa@gmail.com>
2024-07-19 18:51:37 +0330

    golang variable declaring

--- golang/main.go
+++ golang/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	var name string = "Name"
+	var age int = 222
+	name_1 := "Name 1"
+	age_1 := 222
+
+	fmt.Println(name)
+	fmt.Println(name_1)
+	fmt.Println(age)
+	fmt.Println(age_1)
 	fmt.Println("Hello World")
 }
