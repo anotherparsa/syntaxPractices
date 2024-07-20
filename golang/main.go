fa84d4834ec57fb0e40c57b6003004f9ff2d80bb
anotherparsa <anotherparsa@gmail.com>
2024-07-20 19:49:24 +0330

    golang nested if statement

--- golang/main.go
+++ golang/main.go
@@ -6,11 +6,21 @@ import (
 
 func main() {
 
-	age := 20
+	age := 3
+	is_healthy := false
+
 	if age >= 18 {
-		fmt.Println("You're in legal age")
+		if is_healthy {
+			fmt.Println("You're in legal age and healthy")
+		} else {
+			fmt.Println("You're in legal age but not healthy")
+		}
 	} else {
-		fmt.Println("You're not in legal age")
+		if is_healthy {
+			fmt.Println("Youre not in legal age but are healthy")
+		} else {
+			fmt.Println("You're neither in legal age nor healty")
+		}
 	}
 
 }
