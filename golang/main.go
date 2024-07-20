97b95b3be7c2481f7b7324c10ad1841fd64a0e8e
anotherparsa <anotherparsa@gmail.com>
2024-07-20 04:54:27 +0330

    golang separate files but the same package

--- golang/main.go
+++ golang/main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Name"
-	var age int = 222
-	name_1 := "Name 1"
-	age_1 := 222
+
 
 	fmt.Println(name)
 	fmt.Println(name_1)
---
+++ golang/variables.go
@@ -0,0 +1,6 @@
+package main
+
+var name string = "Name"
+var age int = 222
+var name_1 = "Name 1"
+var age_1 = 222
