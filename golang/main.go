72320b2d1320f6b373f1600e409963d68b8559eb
anotherparsa <anotherparsa@gmail.com>
2024-07-21 14:29:49 +0330

    Golang iterating over an array with for loop

--- golang/main.go
+++ golang/main.go
@@ -10,9 +10,11 @@ func main() {
 	names[0] = "Name 1"
 	names[1] = "Name 2"
 	names[2] = "Name 3"
-	fmt.Println(names[1])
 
-	numbers := [4]int{1, 2, 3, 4}
-	fmt.Println(numbers[0])
+	//	numbers := [4]int{1, 2, 3, 4}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
 
 }
