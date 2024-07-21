8e877a3ae2c357d64ccbf338730060e3384c5511
anotherparsa <anotherparsa@gmail.com>
2024-07-21 14:42:36 +0330

    Golang two dimentional array

--- golang/main.go
+++ golang/main.go
@@ -6,23 +6,8 @@ import (
 
 func main() {
 
-	var names [3]string
-	names[0] = "Name 1"
-	names[1] = "Name 2"
-	names[2] = "Name 3"
-
-	//	numbers := [4]int{1, 2, 3, 4}
-
-	names2 := names
-	names3 := &names
-	fmt.Println(names[0])
-	fmt.Println(names2[0])
-	fmt.Println(names3[0])
-	names2[0] = "Changed"
-	fmt.Println(names2[0])
-	fmt.Println(names[0])
-	names3[0] = "changed"
-	fmt.Println(names3[0])
-	fmt.Println(names[0])
+	twodimentionalarray := [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	fmt.Println(twodimentionalarray[0][2]) // 3
+	fmt.Println(twodimentionalarray[1][1]) // 5
 
 }
