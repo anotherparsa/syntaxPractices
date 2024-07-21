065cb99f9cfbeeb91b73716768123688b02b8fbf
anotherparsa <anotherparsa@gmail.com>
2024-07-21 15:08:15 +0330

    Appending Element to a slice

--- golang/main.go
+++ golang/main.go
@@ -8,5 +8,13 @@ func main() {
 	fmt.Println(slice1[0])
 	slice2 := []int{1, 2, 3, 4, 5, 6}
 	fmt.Println(slice2[3])
+	var makeslice = make([]int, 4, 7)
+	fmt.Println(makeslice)
+	makeslice[0] = 0
+	makeslice[1] = 1
+	makeslice[2] = 2
+	makeslice[3] = 3
+	makeslice = append(makeslice, 4, 5, 6)
+	fmt.Println(makeslice)
 
 }
