365d7301fa8518a090ccb1a0b23ea3d7bffb7791
anotherparsa <anotherparsa@gmail.com>
2024-07-25 17:56:22 +0330

    golang function return type

--- golang/main.go
+++ golang/main.go
@@ -6,8 +6,13 @@ import (
 
 func main() {
 	greeting("Alex")
+	fmt.Println(mtultiple(2, 3))
 }
 
 func greeting(name string) {
 	fmt.Printf("Hello %v \n", name)
+}
+
+func mtultiple(a int, b int) int {
+	return a * b
 }
