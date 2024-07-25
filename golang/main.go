10560ec9aea4c8532d8a4e10c91c1e9a648157f5
anotherparsa <anotherparsa@gmail.com>
2024-07-25 18:06:09 +0330

    golang function with multiple returns

--- golang/main.go
+++ golang/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	square := func(x float64) float64 {
-		return math.Sqrt(x)
-	}
-	//why?
-	fmt.Println(square(9))
+	sum, avg := multiplereturn(2, 10)
+	fmt.Printf("This is sum %v and this is average %v \n", sum, avg)
+}
+
+func multiplereturn(a int, b int) (int, int) {
+	sum := a + b
+	avg := sum / 2
+
+	return sum, avg
 }
