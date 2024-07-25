13bb22471e3f4f9bee7ebf6deefa3d78a25d442e
anotherparsa <anotherparsa@gmail.com>
2024-07-25 17:46:46 +0330

    simple function

--- golang/main.go
+++ golang/main.go
@@ -5,27 +5,9 @@ import (
 )
 
 func main() {
+	greeting("Alex")
+}
 
-	emptyMap := map[int]string{}
-	emptyMap[1] = "One"
-	emptyMap[2] = "Two"
-	fmt.Println(emptyMap[1])
-
-	initializedmap := map[int]string{1: "One", 2: "Two", 3: "Three", 4: "Four"}
-
-	fmt.Println(initializedmap)
-	//updating
-	initializedmap[4] = "Chahar"
-	fmt.Println(initializedmap)
-	//adding
-	initializedmap[5] = "Five"
-	initializedmap[6] = "Six"
-	fmt.Println(initializedmap)
-	delete(initializedmap, 6)
-	fmt.Println(initializedmap)
-
-	for key, _ := range initializedmap {
-		fmt.Println(initializedmap[key])
-	}
-
+func greeting(name string) {
+	fmt.Printf("Hello %v \n", name)
 }
