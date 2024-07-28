b3c85d343b8758f9a7de7ed9b50e3df4730cc8e9
anotherparsa <anotherparsa@gmail.com>
2024-07-28 22:42:17 +0330

    creating a struct

--- golang/main.go
+++ golang/main.go
@@ -1,26 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+type address struct {
+	state   string
+	city    string
+	zipcode int
+}
 
 func main() {
-
-	simpleint := 10
-	simplestring := "Hello"
-	var intpointer *int = &simpleint
-	var stringpointer *string = &simplestring
-	fmt.Println(intpointer)
-	fmt.Println(stringpointer)
-	fmt.Println(*intpointer)
-	fmt.Println(*stringpointer)
-	var doubleintpointer **int = &intpointer
-	var doublestringpointer **string = &stringpointer
-	fmt.Println(doubleintpointer)
-	fmt.Println(doublestringpointer)
-	fmt.Println(*doubleintpointer)
-	fmt.Println(*doublestringpointer)
-	fmt.Println(**doubleintpointer)
-	fmt.Println(**doublestringpointer)
 
 }
