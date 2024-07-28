dcd9fa02a3ffffb8bed93d3638a0ecc5be9ed47d
anotherparsa <anotherparsa@gmail.com>
2024-07-29 02:41:10 +0330

    separate handler function

--- golang/main.go
+++ golang/main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world")
-	})
+	http.HandleFunc("/", homePageHandler)
 	fmt.Println("Runnin server on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello This is the home page")
+}
