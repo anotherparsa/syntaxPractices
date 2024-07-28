beac0c84defdb8d2df130622f4f3714aaa7d71c0
anotherparsa <anotherparsa@gmail.com>
2024-07-28 22:45:40 +0330

    methods of a struct

--- golang/main.go
+++ golang/main.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 type address struct {
 	state   string
 	city    string
 	zipcode int
+}
+
+func (a address) show() {
+	fmt.Printf("Tthe state is %v and the city is %v and the zipcode is %v \n", a.state, a.city, a.zipcode)
 }
 
 func main() {
@@ -11,7 +17,5 @@ func main() {
 	a.city = "Some City"
 	a.state = "Some State"
 	a.zipcode = 12345
-
-	fmt.Println(a.zipcode)
-	fmt.Println(a.state)
+	a.show()
 }
