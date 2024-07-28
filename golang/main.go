4a601dcb71bbc6435b42c1f5cc38c4733bc39255
anotherparsa <anotherparsa@gmail.com>
2024-07-29 02:25:31 +0330

    cleaning

--- golang/main.go
+++ golang/main.go
@@ -1,21 +1,7 @@
 package main
 
-import "fmt"
-
-type address struct {
-	state   string
-	city    string
-	zipcode int
-}
-
-func (a address) show() {
-	fmt.Printf("Tthe state is %v and the city is %v and the zipcode is %v \n", a.state, a.city, a.zipcode)
-}
+import()
 
 func main() {
-	var a address
-	a.city = "Some City"
-	a.state = "Some State"
-	a.zipcode = 12345
-	a.show()
+	
 }
