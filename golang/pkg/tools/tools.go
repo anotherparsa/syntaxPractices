710915d87542017885a7bd37f506342169640af6
anotherparsa <anotherparsa@gmail.com>
2024-08-01 00:52:09 +0330

    using layout template

--- golang/pkg/tools/tools.go
+++ golang/pkg/tools/tools.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplates(w http.ResponseWriter, templateAddress string, data any) {
-	parsedTemplate, err := template.ParseFiles(templateAddress)
+	parsedTemplate, err := template.ParseFiles(templateAddress, "../../pkg/baseTemplate/baseTemplate.html")
 	if err != nil {
 		fmt.Println(err)
 	} else {
