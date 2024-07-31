dfbd249292bb4c1575c495a7e32b5aa4a0a7aca5
anotherparsa <anotherparsa@gmail.com>
2024-08-01 01:12:33 +0330

    simple caching system

--- golang/pkg/tools/tools.go
+++ golang/pkg/tools/tools.go
@@ -6,11 +6,56 @@ import (
 	"net/http"
 )
 
-func RenderTemplates(w http.ResponseWriter, templateAddress string, data any) {
+/*func RenderTemplates(w http.ResponseWriter, templateAddress string, data any) {
 	parsedTemplate, err := template.ParseFiles(templateAddress, "../../pkg/baseTemplate/baseTemplate.html")
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		parsedTemplate.Execute(w, data)
 	}
+}*/
+
+var templateCache = make(map[string]*template.Template)
+
+func RenderTemplates(w http.ResponseWriter, templateAddress string, data any) {
+	var template *template.Template
+	var err error
+
+	//check if we already have a parsed template
+	_, inMap := templateCache[templateAddress]
+	if !inMap {
+		//need to parse from scratch
+		fmt.Println("It wasn't in the cache and now we're creating and adding it to the map")
+		err = createTemplateCache(templateAddress)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		//we have a parsed template
+		fmt.Println("Using parsed template")
+	}
+
+	template = templateCache[templateAddress]
+	err = template.Execute(w, data)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func createTemplateCache(templateAddress string) error {
+	templates := []string{
+		templateAddress,
+		"../../pkg/baseTemplate/baseTemplate.html",
+	}
+	//parse the template
+	parsedTemplate, err := template.ParseFiles(templates[0], templates[1])
+
+	if err != nil {
+		return err
+	} else {
+		//add parsed template to the cache map
+		templateCache[templateAddress] = parsedTemplate
+		return nil
+	}
+
 }
